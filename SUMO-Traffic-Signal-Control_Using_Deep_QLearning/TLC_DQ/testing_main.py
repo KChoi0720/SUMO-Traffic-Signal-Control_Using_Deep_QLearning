d3ae5ce2f81c0ae106232c5b2b59bcb94fb2f210
KevinC <choicanlight@gmail.com>
2024-04-03 23:15:58 +1300

    Modify testing_main.py

--- SUMO-Traffic-Signal-Control_Using_Deep_QLearning/TLC_DQ/testing_main.py
+++ SUMO-Traffic-Signal-Control_Using_Deep_QLearning/TLC_DQ/testing_main.py
@@ -13,7 +13,7 @@ from utils import import_test_configuration, set_sumo, set_test_path
 
 if __name__ == "__main__":
 
-    config = import_test_configuration(config_file='/Users/cuijiping/Desktop/Github/Deep-QLearning-Agent-for-Traffic-Signal-Control/TLCS/testing_settings.ini')
+    config = import_test_configuration(config_file='/Users/cuijiping/Desktop/Github/UMO-Traffic-Signal-Control_Using_Deep_QLearning/TLC_DQ/testing_settings.ini')
     sumo_cmd = set_sumo(config['gui'], config['sumocfg_file_name'], config['max_steps'])
     model_path, plot_path = set_test_path(config['models_path_name'], config['model_to_test'])
 
